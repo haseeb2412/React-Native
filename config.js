48872a318a67f4f645e33cef12a5fa2a52829966
haseeb2412 <haseeb241202@gmail.com>
2024-01-26 22:03:56 +0500

    complete

--- config.js
+++ config.js
@@ -4,13 +4,7 @@ import 'firebase/compat/firestore'
 
 
 const firebaseConfig={
-  apiKey: "AIzaSyD6tvMLBqZ3oLMIQAAyNsHiATIu6Inhn0Q",
-  authDomain: "mytime-2a277.firebaseapp.com",
-  projectId: "mytime-2a277",
-  storageBucket: "mytime-2a277.appspot.com",
-  messagingSenderId: "467936141849",
-  appId: "1:467936141849:web:2ff3b4cbfefbefa1a38088",
-  measurementId: "G-JSBENMGK2D"
+  
   }
 
   if(!firebase.apps.length){
