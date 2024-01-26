bbdcf1604d2e9077f1468678ff7ec84c23615b7b
haseeb2412 <haseeb241202@gmail.com>
2024-01-26 22:01:27 +0500

    Complete

--- Navigation/AppNavigation.js
+++ Navigation/AppNavigation.js
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProfileScreen from '../screens/ProfileScreen';
@@ -8,21 +8,66 @@ import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import ReasonScreen from '../screens/ReasonScreen';
 import MyProfile from '../screens/MyProfile';
+import {firebase} from '../config'
+import SignUpScreen from '../screens/SignupScreen';
 
 const Stack = createNativeStackNavigator();
 
 
 export default function AppNavigation() {
+
+
+
+  const [initialize, setInitialize] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initialize) setInitialize(false);
+  }
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initialize) return null;
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Welcome'>
+
+
+
+
+<Stack.Navigator initialRouteName='Welcome'> 
+        {user ? (
+          <>
+          <Stack.Screen name="Profile" options={{headerShown: false}} component={ProfileScreen} />
+          <Stack.Screen name="MyProfile" options={{headerShown: false}} component={MyProfile} />
+          <Stack.Screen name="Reason" options={{headerShown: false}} component={ReasonScreen} />
+          </>
+        ) : (
+          <>
+                <Stack.Screen name="Welcome" options={{headerShown: false}} component={WelcomeScreen} />
+                <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
+    <Stack.Screen name="SignUp" options={{headerShown: false}} component={SignUpScreen} />
+    {/* <Stack.Screen name="MyProfile" options={{headerShown: false}} component={MyProfile} />
+    <Stack.Screen name="Reason" options={{headerShown: false}} component={ReasonScreen} /> */}
+          </>
+        )}
+      </Stack.Navigator>
+
+
+
+
+
+      {/* <Stack.Navigator initialRouteName='Welcome'>
         <Stack.Screen name="Profile" options={{headerShown: false}} component={ProfileScreen} />
         <Stack.Screen name="Welcome" options={{headerShown: false}} component={WelcomeScreen} />
         <Stack.Screen name="Login" options={{headerShown: false}} component={LoginScreen} />
         <Stack.Screen name="SignUp" options={{headerShown: false}} component={SignupScreen} />
         <Stack.Screen name="Reason" options={{headerShown: false}} component={ReasonScreen} />
         <Stack.Screen name="MyProfile" options={{headerShown: false}} component={MyProfile} />
-      </Stack.Navigator>
+      </Stack.Navigator> */}
     </NavigationContainer>
   )
 }
